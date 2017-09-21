568de1aa1c7a4bad1dda695c05bb27d22fe4bf63
benedikt <benedikthth@gmail.com>
2017-09-21 11:42:38 +0000

    first commit

---
+++ hlexer.h
@@ -0,0 +1,18 @@
+// Compilers Fall 2017. Project -- part I.
+#ifndef LEXER_HLEXER_H
+#define LEXER_HLEXER_H
+
+#include "lexer.h"
+
+class HLexer : public Lexer {
+public:
+    HLexer( std::istream& is, SymbolTable& symbol_table );
+    virtual void get_next( Token& token );
+    virtual std::string get_name() const;
+    virtual ~HLexer();
+private:
+    char c_;
+    int  line_no_;
+};
+
+#endif //LEXER_HLEXER_H
